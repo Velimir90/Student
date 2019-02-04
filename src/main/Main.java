107c317cea7d1ff3584c81c0efae27d3f4285162
Velja <velja@Nemanjas-MacBook-Pro.local>
2019-02-05 00:56:21 +0100

    napravljena klasa Student

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import student.Student;
+
 public class Main {
     
     public static void main(String[] args) {
-
+        Student student = new Student();
+        student.info();
         
     }
     
--- src/student/Student.java
+++ src/student/Student.java
@@ -5,4 +5,50 @@ public class Student {
     
     private String firstName;
     private String lastName;
+    private int yearOfBirth;
+    
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+    
+    public Student() {
+        this.firstName = "Velimir";
+        this.lastName = "Markovic";
+        this.yearOfBirth = 1990;
+    }
+
+    public Student(String firstName, String lastName, int yearOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+    } 
+    
+    public void info() {
+        System.out.println("First Name: " + this.firstName);
+        System.out.println("Last Name: " + this.lastName);
+        System.out.println("Year of birth: " + this.yearOfBirth);
+        System.out.println("");
+    }
+    
+    
+    
 }
